e91fae041a34e2f3d012e4270743a74dd7ba8b77
Amit Kumar <Soulreaper@Amits-MacBook-Pro.local>
2018-11-10 14:44:40 +0530

    final commit :tada:

--- routes/tweets.js
+++ routes/tweets.js
@@ -2,6 +2,7 @@ const request = require('request');
 const Tweet = require('../models/tweet');
 const User = require('../models/user');
 const chalk = require('chalk');
+const Follow = require('../models/follow');
 
 module.exports = function(app) {
     app.post('/tweet', function(req, res) {
@@ -30,7 +31,30 @@ module.exports = function(app) {
         }
     });
 
-    app.get('/usertweets', function(req, res) {
-
+    app.get('/usertweets/:uid', function(req, res) {
+        let userID = req.params.uid;
+        if (userID) {
+            // userID belongs to the follower 
+            followee_set = [];
+            tweet_set = [];
+            Follow.find({follower : userID}, (err, resp) => {
+                if (err) throw err;
+                for (let i = 0; i < resp.length; i++) {
+                    if (resp[i].end == undefined){
+                        followee_set.push(resp[i].followee);
+                    }
+                }
+                if (followee_set) {
+                  Tweet.find({ "userID" : { "$in" : followee_set} }).sort("-tweeted_at")
+                  .exec((err, data) => {
+                    if(err){
+                        return console.log(err);
+                    } else {
+                        res.status(200).send(data);
+                    }
+                  })
+                }
+            })
+        }
     });
 }
